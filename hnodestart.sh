328624bafea3139bc18f5d5d971121e29577eaac
Chainode Capital <53407923+Chainode@users.noreply.github.com>
2019-09-02 16:52:20 +0200

    Update hnodestart.sh

--- hnodestart.sh
+++ hnodestart.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/expect -f
 
 # This script starts node.sh and gives passphrase automatically
+# You have to know that is not recommended to store bls key on your validator, especially if is not secure. 
+# In this case you could use only the alert part and restart your node manually, once you receive the alerts.
 
 # Edit the passphrase:
 set pass "your bls passphrase"
